0d6de9689b3233f80899e541cb98e2183377ebb3
OberKnirps <fabian.stroschke@campus.tu-berlin.de>
2023-01-31 12:37:57 +0100

    Refactored modifiedGrahamScan into method

--- main.cpp
+++ main.cpp
@@ -4,43 +4,82 @@
 #include "glm/glm.hpp"
 namespace plt = matplotlibcpp;
 
-#define showMatPlot false
+#define showMatPlot true
+#define showTimeStamps false
 
+struct angleVec2Pair{
+    double angle;
+    glm::vec2 vec;
 
-double graham_scan(glm::vec2 p0, glm::vec2 a, glm::vec2 b){
-    //glm::mat3x3 mat = {{1.0,1.0,1.0},{},{}}
-}
-std::vector<std::pair<double, glm::vec2>> generateSortedAngleMap(const std::vector<glm::vec2> &points,const glm::vec2 &fixpoint){
-    std::vector<std::pair<double, glm::vec2>> list;
-    for(auto &p: points){
-        list.emplace_back(atan2(fixpoint.y-p.y,fixpoint.x-p.x)*180/M_PI+180,p);
+    angleVec2Pair(double a, glm::vec2 v) : angle(a), vec(v){};
+
+    bool operator<(const angleVec2Pair &other) const
+    {
+        return angle < other.angle;
     }
-    return list;
-}
+};
 
-int main() {
-    std::vector<glm::vec2> points;
-    int sample_size =1000000;
-    int seed = 1234;//std::time(nullptr);
-    std::srand(seed);
+template<typename PointType>
+struct Input{
+    std::vector<PointType> pointCloud;
+    PointType fixPoint{};
+};
+
+void modifiedGrahamScanVec2(std::vector<glm::vec2> pointCloud, glm::vec2 fixPoint);
+
+enum fixPointLocation {FPL_CONVEXHULL, FPL_RANDOM, FPL_USUALLY_INSIDE, FPL_CENTER};
+
+Input<glm::vec2> generateInputVec2(int sample_size, int seed = std::time(nullptr),fixPointLocation hint = FPL_RANDOM, size_t x_dim = 100, size_t y_dim = 100) {
+    Input<glm::vec2> res;
+
     //generate point cloud and fixpoint
-    int rand =std::rand();
-    for (int i = 0; i<sample_size; i++) {
-        points.emplace_back(sin(std::rand()) * 50, cos(std::rand()) * 50);
+    srand(seed);
+    int r = std::rand();
+    double scale = 1;
+
+    switch (hint) {
+        case FPL_CONVEXHULL:
+            res.fixPoint = glm::vec2(sin(r) * x_dim, cos(r) * y_dim);
+            scale = 0.7;
+            break;
+        case FPL_RANDOM:
+            res.fixPoint = glm::vec2((float )(rand() % (x_dim * 2)) - x_dim, (float )(rand() % (y_dim * 2)) - y_dim);
+            break;
+        case FPL_USUALLY_INSIDE:
+            res.fixPoint = glm::vec2(sin(r) * x_dim/2, cos(r) * y_dim/2);
+            break;
+        case FPL_CENTER:
+            res.fixPoint = glm::vec2(0,0);
+            break;
     }
-    glm::vec2 fixPoint = glm::vec2(sin(rand)*100,cos(rand)*100);//(std::rand()%2000)/10.0,(std::rand()%2000)/10.0);
 
+    for (int i = 0; i<sample_size; i++) {
+        r = rand();
+        res.pointCloud.emplace_back(sin(r) * (rand()%x_dim)*scale, cos(r) * (rand()%y_dim)*scale);
+    }
+    return res;
+}
+
+
+
+void modifiedGrahamScanVec2(std::vector<glm::vec2> pointCloud, glm::vec2 fixPoint) {
+#if showTimeStamps
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    auto map = generateSortedAngleMap(points,fixPoint);
-    end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mircos]" << std::endl;
-    std::sort(map.begin(),map.end(),[](std::pair<double, glm::vec2> &a, std::pair<double, glm::vec2> &b) {return a.first < b.first; });
-    end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mircos]" << std::endl;
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+#endif
 
+    std::vector<std::vector<angleVec2Pair>> vectorList(360);
+    for(auto &p: pointCloud){
+        auto angle = atan2(fixPoint.y-p.y,fixPoint.x-p.x)*180/M_PI+180;
+        vectorList[(int)angle].emplace_back(angle,p);
+    }
+
+#if showTimeStamps
+    end = std::chrono::steady_clock::now();
+        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mircos]" << std::endl;
+#endif
 #if showMatPlot
-    //convert vectors for matplot
+//convert vectors for matplot
     std::vector<float> x1;
     std::vector<float> y1;
     x1.emplace_back(fixPoint.x);
@@ -55,76 +94,92 @@ int main() {
     line.second.emplace_back(fixPoint.y);
     line.second.emplace_back(fixPoint.y);
 #endif
-    //  plt::ion();
-    // plt::plot(x, y, {{"linewidth","0.0" }, {"marker", "o"}});
-    double min =map.begin()->first;
-    double max =map.begin()->first;
-    double last =map.begin()->first;
-    double angle =max-min;
-    for(auto [k,v]: map){
-#if showMatPlot
-        x.emplace_back(v.x);
-        y.emplace_back(v.y);
-        line.first[1]=v.x;
-        line.second[1]=v.y;
-        //std::cout << k<<"\n";
-        plt::clf();
-        plt::ylim(-120,120);
-        plt::xlim(-120,120);
-        plt::plot(x, y, {{"linewidth","0.0" }, {"marker", "x"},{"markersize", "2.5"}});
-        plt::plot(x1,y1,{{"linewidth","0.0" }, {"marker", "o"},{"markerfacecolor","r"},{"markeredgecolor","r"}});
-        plt::plot(line.first,line.second,{{"linewidth","0.5" }});
+//  plt::ion();
+// plt::plot(x, y, {{"linewidth","0.0" }, {"marker", "o"}});
+    int offset = -1;
+    double min =0;
+    double max =0;
+    double angle =0;
+    for(int i= 0; i<vectorList.size(); i++){
+        #if showMatPlot
+            for(auto &[a,v]:vectorList[i]) {
+                x.emplace_back(v.x);
+                y.emplace_back(v.y);
+                line.first[1] = v.x;
+                line.second[1] = v.y;
+                //std::cout << k<<"\n";
+                plt::clf();
+                plt::ylim(-120, 120);
+                plt::xlim(-120, 120);
+                plt::plot(x, y, {{"linewidth",  "0.0"},
+                                 {"marker",     "x"},
+                                 {"markersize", "2.5"}});
+                plt::plot(x1, y1, {{"linewidth",       "0.0"},
+                                   {"marker",          "o"},
+                                   {"markerfacecolor", "r"},
+                                   {"markeredgecolor", "r"}});
+                plt::plot(line.first, line.second, {{"linewidth", "0.5"}});
 
-        plt::draw();
-        plt::pause(0.001);
-#endif
-        if(k-last>angle) {
-            angle = k - last;
-            min = last;
-            max = k;
+                plt::draw();
+                plt::pause(0.001);
+            }
+        #endif
+        if(not vectorList[i].empty()){
+            if(offset == -1) offset = i;
+            if(angle>max-min) {
+                min = i - angle;
+                max = i;
+            }
+            angle = 0;
         }
-        last = k;
-    }
-    if(map.begin()->first-(last-360)>angle) {
-        angle = map.begin()->first-(last-360);
-        min = map.begin()->first;
-        max = last;
-    }
-    end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mircos]" << std::endl;
-#if showMatPlot
-    glm::vec2 vmin;
-    glm::vec2 vmax;
-    
-    for(auto &p: map){
-        if(p.first == min) vmin=p.second;
-        if(p.first == max) vmax=p.second;
-    }
+        angle++;
 
+    }
+    if(angle+offset>max-min) {
+        min = vectorList.size()-angle;
+        max = offset;
+    }
+    angleVec2Pair vmin = *(std::max_element(vectorList[min].begin(),vectorList[min].end()));
+    angleVec2Pair vmax = *(std::min_element(vectorList[max].begin(),vectorList[max].end()));
+
+#if showTimeStamps
+    end = std::chrono::steady_clock::now();
+        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mircos]" << std::endl;
+#endif
+#if showMatPlot
     std::pair<std::vector<float>,std::vector<float>> res;
-    res.first.emplace_back(vmin.x+(vmin.x-fixPoint.x));
+    res.first.emplace_back(vmin.vec.x+(vmin.vec.x-fixPoint.x));
     res.first.emplace_back(fixPoint.x);
-    res.first.emplace_back(vmax.x+(vmax.x-fixPoint.x));
-    res.second.emplace_back(vmin.y+(vmin.y-fixPoint.y));
+    res.first.emplace_back(vmax.vec.x+(vmax.vec.x-fixPoint.x));
+    res.second.emplace_back(vmin.vec.y+(vmin.vec.y-fixPoint.y));
     res.second.emplace_back(fixPoint.y);
-    res.second.emplace_back(vmax.y+(vmax.y-fixPoint.y));
+    res.second.emplace_back(vmax.vec.y+(vmax.vec.y-fixPoint.y));
     plt::plot(res.first,res.second,{{"linewidth","1.5" }});
 
     plt::show();
 #endif
 
-    for(auto [k,v]: map){
-        if(k == min or k == max){
-            std::cout << k << "\n";
-            break;
-        }
-    }
-    std::cout << angle << "\n";
+    auto theta = vmax.angle-vmin.angle;
+    if(vmax.angle<vmin.angle) theta = 360 +vmax.angle-vmin.angle;
+    std::cout << vmax.angle<< "|" <<vmin.angle << "\n";
+    std::cout << "Angle:" <<theta<< "\n";
     std::cout << fixPoint.x << "|" << fixPoint.y << "\n";
-    std::cout << seed << "\n";
-
+#if showTimeStamps
     end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mircos]" << std::endl;
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[mircos]" << std::endl;
+        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+#endif
+}
+
+
+int main() {
+    int sample_size =100;
+    int seed = std::time(nullptr);
+
+    Input<glm::vec2> input = generateInputVec2(sample_size, seed, FPL_RANDOM);
+    modifiedGrahamScanVec2(input.pointCloud, input.fixPoint); //TODO output solution vectors
+
+
+    std::cout << seed << "\n";
 }
