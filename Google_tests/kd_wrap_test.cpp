21173fdaa3b8fc4884276610e162135b55736e3e
FabianStroschke <fabian.stroschke@campus.tu-berlin.de>
2023-07-20 13:37:37 +0200

    added tests

---
+++ Google_tests/kd_wrap_test.cpp
@@ -0,0 +1,100 @@
+//
+// Created by fabia on 22.06.2023.
+//
+#include "gtest/gtest.h"
+#include "kd_wrap.h"
+#include "input_generators.h"
+
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/convex_hull_3.h>
+
+//typedefs
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron_3;
+
+void testSameConvexHullRandomKD(long seed, long nPoints){
+
+    //generate input
+    auto input = generateInputVec3(nPoints, seed, FPL_CONVEXHULL);
+
+    //build convex hull
+    Polyhedron_3 poly;
+    CGAL::convex_hull_3(input.pointCloud.begin(), input.pointCloud.end(), poly);
+
+    //build convex mesh
+    Mesh mesh;
+    KDWrap(input.pointCloud, mesh);
+
+    for(auto &p: poly.points()){
+        EXPECT_NE(std::find(mesh.points().begin(), mesh.points().end(),p),mesh.points().end());
+    }
+}
+
+void testSameConvexHullInputKD(std::string path){
+    //generate input
+    auto input = readInputVec3(path);
+
+    //build convex hull
+    Polyhedron_3 poly;
+    CGAL::convex_hull_3(input.pointCloud.begin(), input.pointCloud.end(), poly);
+
+    //build convex mesh
+    Mesh mesh;
+    KDWrap(input.pointCloud, mesh);
+
+    for(auto &p: poly.points()){
+        EXPECT_NE(std::find(mesh.points().begin(), mesh.points().end(),p),mesh.points().end())<<"Failed with object: " << path;
+    }
+}
+
+TEST(CheckConvexHullKD, N500){
+    int n = 500;
+    testSameConvexHullRandomKD(1, n);
+    testSameConvexHullRandomKD(13, n);
+    testSameConvexHullRandomKD(345, n);
+    testSameConvexHullRandomKD(7345, n);
+    testSameConvexHullRandomKD(98175, n);
+    testSameConvexHullRandomKD(123456, n);
+    testSameConvexHullRandomKD(9807, n);
+    testSameConvexHullRandomKD(468014, n);
+}
+
+TEST(CheckConvexHullKD, N5000){
+    int n = 5000;
+    testSameConvexHullRandomKD(1, n);
+    testSameConvexHullRandomKD(13, n);
+    testSameConvexHullRandomKD(345, n);
+    testSameConvexHullRandomKD(7345, n);
+    testSameConvexHullRandomKD(98175, n);
+    testSameConvexHullRandomKD(123456, n);
+    testSameConvexHullRandomKD(9807, n);
+    testSameConvexHullRandomKD(468014, n);
+}
+
+TEST(CheckConvexHullKD, N50000){
+    int n = 50000;
+    testSameConvexHullRandomKD(1, n);
+    testSameConvexHullRandomKD(13, n);
+    testSameConvexHullRandomKD(345, n);
+    testSameConvexHullRandomKD(7345, n);
+    testSameConvexHullRandomKD(98175, n);
+    testSameConvexHullRandomKD(123456, n);
+    testSameConvexHullRandomKD(9807, n);
+    testSameConvexHullRandomKD(468014, n);
+}
+
+TEST(CheckConvexHullKD, Objects){
+    testSameConvexHullInputKD("../inputs/alligator.obj");
+    testSameConvexHullInputKD("../inputs/armadillo.obj");
+    testSameConvexHullInputKD("../inputs/beast.obj");
+    testSameConvexHullInputKD("../inputs/beetle.obj");
+    testSameConvexHullInputKD("../inputs/beetle-alt.obj");
+    testSameConvexHullInputKD("../inputs/bimba.obj");
+    testSameConvexHullInputKD("../inputs/cow.obj");
+    testSameConvexHullInputKD("../inputs/fandisk.obj");
+    testSameConvexHullInputKD("../inputs/happy.obj");
+    testSameConvexHullInputKD("../inputs/homer.obj");
+    testSameConvexHullInputKD("../inputs/horse.obj");
+    testSameConvexHullInputKD("../inputs/igea.obj");
+    testSameConvexHullInputKD("../inputs/lucy.obj");
+    testSameConvexHullInputKD("../inputs/orge.obj");
+}
--- Google_tests/tree_helper_test.cpp
+++ Google_tests/tree_helper_test.cpp
@@ -14,7 +14,7 @@ typedef CGAL::Polyhedron_3<Kernel> Polyhedron_3;
 void testFindBoundary(long seed, long nPoints, int bucketSize){
 
     //generate input
-    auto input = generateInputVec3(nPoints, 12345, FPL_CONVEXHULL);
+    auto input = generateInputVec3(nPoints, seed, FPL_CONVEXHULL);
 
     //build octree
     Octree octree(input.pointCloud);
@@ -60,6 +60,64 @@ void testFindBoundary(long seed, long nPoints, int bucketSize){
               << "\nTests Completed:" << tests_complete
               << "\nTests Failed:   " << tests_failed
               << "\nTests NAN:      " << tests_nan;
+}
+
+void testFindBoundaryKD(long seed, long nPoints){
+
+    //generate input
+    auto input = generateInputVec3(nPoints, seed, FPL_CONVEXHULL);
+
+    //build octree
+    Kd_tree kd_tree(input.pointCloud.begin(),input.pointCloud.end(), Kd_tree::Splitter(1));
+    kd_tree.build();
+
+    //build convex hull
+    Polyhedron_3 poly;
+    CGAL::convex_hull_3(input.pointCloud.begin(), input.pointCloud.end(), poly);
+    long tests_total = 0;
+    long tests_failed = 0;
+    long tests_complete = 0;
+    long tests_nan = 0;
+
+    //iterate over faces
+    for(auto it = poly.facets_begin(); it != poly.facets_end(); it++){
+        auto face_iterator = *it->facet_begin();
+        if(face_iterator.face()->is_triangle()){
+            tests_total += 3;
+            Kernel::Point_3 vertices[3] = {
+                    face_iterator.vertex()->point(),
+                    face_iterator.next()->vertex()->point(),
+                    face_iterator.next()->next()->vertex()->point()
+            };
+            for (int i = 0; i < 3; ++i) {
+                auto res = findBoundaryPoint(kd_tree, {vertices[i],vertices[(i+1)%3]},BS_LEFT);
+                if(res != nullptr){
+                    EXPECT_EQ(*res, vertices[(i+2)%3]) << "Wrong solution. At edge: " << "{" << vertices[i] << "},{" << vertices[(i+1)%3] <<"} Seed: "<< seed;
+                    if(*res == vertices[(i+2)%3]){
+                        tests_complete++;
+                    }else{
+                        tests_failed++;
+                        /*Kernel::Plane_3 p1(vertices[i],vertices[(i+1)%3],vertices[(i+2)%3]);
+                        Kernel::Plane_3 p2(vertices[i],vertices[(i+1)%3],*res);
+                        std::cout << "Angle:     " << acos((p1.orthogonal_vector()*p2.orthogonal_vector())
+                            /(sqrt(p1.orthogonal_vector().squared_length())*sqrt(p2.orthogonal_vector().squared_length())));*/
+
+                    }
+                }else{
+                    EXPECT_TRUE(res) << "No solution found";
+                    tests_nan++;
+                }
+            }
+        }else{
+            EXPECT_TRUE(face_iterator.face()->is_triangle()) << "Face has more than 3 Vertices.";
+        }
+    }
+    if(tests_failed){
+        std::cout << "Total Test:     " << tests_total
+                  << "\nTests Completed:" << tests_complete
+                  << "\nTests Failed:   " << tests_failed
+                  << "\nTests NAN:      " << tests_nan;
+    }
 }
 
 TEST(FindBoundary, C5B1){
@@ -121,5 +179,21 @@ TEST(FindBoundary, C50000B15){
 TEST(FindBoundary, C50000B50){
     testFindBoundary(1404,500,50);
 }
+TEST(FindBoundaryKD, C500){
+    testFindBoundaryKD(1404,500);
+}TEST(FindBoundaryKD, C5000){
+    testFindBoundaryKD(1404,5000);
+}
+TEST(FindBoundaryKD, C50000){
+    testFindBoundaryKD(1404,50000);
+}
+TEST(FindBoundaryKD, C500000){
+    testFindBoundaryKD(1404,500000);
+}
 
+TEST(FindBoundaryKD, C50M){
+    for(int i = 0; i<1000000; i++){
+        testFindBoundaryKD(i,10);
+    }
+}
 
